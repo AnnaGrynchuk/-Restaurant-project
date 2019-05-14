a2c371e43d3b8a9f9417f791f12a60ec6b79e67c
ann grynchuk <ann.grynchuk7@gmail.com>
2019-05-14 06:45:59 +0300

    Updates

---
+++ precache-manifest.092571992c0baf566092534b9ff33092.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daa44dcaed9b6af17d605b005882a6f",
+    "url": "/Restaurant-project/index.html"
+  },
+  {
+    "revision": "6e3d1a314471200777cf",
+    "url": "/Restaurant-project/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "6b88289e5f600b31219b",
+    "url": "/Restaurant-project/static/js/2.bbec83f2.chunk.js"
+  },
+  {
+    "revision": "6e3d1a314471200777cf",
+    "url": "/Restaurant-project/static/js/main.1e002377.chunk.js"
+  },
+  {
+    "revision": "f83e2d99843e4048f73e",
+    "url": "/Restaurant-project/static/js/runtime~main.aea8c7f5.js"
+  },
+  {
+    "revision": "3bfd61f9fbcd75cd49f29614621c8a0f",
+    "url": "/Restaurant-project/static/media/1.3bfd61f9.jpg"
+  },
+  {
+    "revision": "b833293d815d42e754653930341f38b4",
+    "url": "/Restaurant-project/static/media/1.b833293d.jpg"
+  },
+  {
+    "revision": "09a1d9b6f4b5f50ab17110f8bdd67184",
+    "url": "/Restaurant-project/static/media/10.09a1d9b6.jpg"
+  },
+  {
+    "revision": "26defa1f907d09f934c9780338a2a73a",
+    "url": "/Restaurant-project/static/media/10.26defa1f.jpg"
+  },
+  {
+    "revision": "c29904c5420dc1ce3cef3e528a865101",
+    "url": "/Restaurant-project/static/media/10.c29904c5.jpg"
+  },
+  {
+    "revision": "66d8a8e0f12ebb812750687db8d1051a",
+    "url": "/Restaurant-project/static/media/11.66d8a8e0.jpg"
+  },
+  {
+    "revision": "d99cb84804169a09a529ed8ff3f76c7d",
+    "url": "/Restaurant-project/static/media/11.d99cb848.jpg"
+  },
+  {
+    "revision": "4e63fd3f19d0aebe59f78b7525a18ac3",
+    "url": "/Restaurant-project/static/media/12.4e63fd3f.jpg"
+  },
+  {
+    "revision": "4f2aca7d1a7830150f1c995cdf54eb3f",
+    "url": "/Restaurant-project/static/media/12.4f2aca7d.jpg"
+  },
+  {
+    "revision": "911886dfe09bb96f8e15b481c0a42834",
+    "url": "/Restaurant-project/static/media/13.911886df.jpg"
+  },
+  {
+    "revision": "bb0383095230d48df95885640388ebdd",
+    "url": "/Restaurant-project/static/media/13.bb038309.jpg"
+  },
+  {
+    "revision": "d188d2e0acf9f9e3b46e52160a64647a",
+    "url": "/Restaurant-project/static/media/13.d188d2e0.jpg"
+  },
+  {
+    "revision": "266a89dcb29696879f9e137776598565",
+    "url": "/Restaurant-project/static/media/14.266a89dc.jpg"
+  },
+  {
+    "revision": "dc11bd4d437fc127b12e90b0d0db36af",
+    "url": "/Restaurant-project/static/media/14.dc11bd4d.jpg"
+  },
+  {
+    "revision": "5005786a284b1be14b2f150e006d7492",
+    "url": "/Restaurant-project/static/media/15.5005786a.jpg"
+  },
+  {
+    "revision": "640acc487cab556799381d43dd7b0cf1",
+    "url": "/Restaurant-project/static/media/15.640acc48.jpg"
+  },
+  {
+    "revision": "6875454c73fb3371e50f4a19ea8de822",
+    "url": "/Restaurant-project/static/media/15.6875454c.jpg"
+  },
+  {
+    "revision": "6a16ef66be0b13f48f783560b6c8fff7",
+    "url": "/Restaurant-project/static/media/16.6a16ef66.jpg"
+  },
+  {
+    "revision": "270948e351db6ae90ef1d05522f8113a",
+    "url": "/Restaurant-project/static/media/2.270948e3.jpg"
+  },
+  {
+    "revision": "2c9a33532b29f0008ff0582b7f0f847f",
+    "url": "/Restaurant-project/static/media/2.2c9a3353.jpg"
+  },
+  {
+    "revision": "4021d0392426e259ee0a5829e59c9bea",
+    "url": "/Restaurant-project/static/media/3.4021d039.jpg"
+  },
+  {
+    "revision": "7748668eff01f84f4949970f0f06c387",
+    "url": "/Restaurant-project/static/media/3.7748668e.jpg"
+  },
+  {
+    "revision": "51861c24b910ea6dbf67c3f308401315",
+    "url": "/Restaurant-project/static/media/4.51861c24.jpg"
+  },
+  {
+    "revision": "62b3fa85aeeaf0344bd0ddbe51bdba29",
+    "url": "/Restaurant-project/static/media/4.62b3fa85.JPG"
+  },
+  {
+    "revision": "b0efd8b9bf8ba374b463e2f4cd5371e3",
+    "url": "/Restaurant-project/static/media/4.b0efd8b9.jpg"
+  },
+  {
+    "revision": "69f05f1b3ace80fcde0a598d5471f508",
+    "url": "/Restaurant-project/static/media/5.69f05f1b.jpg"
+  },
+  {
+    "revision": "a722c9d169d41afbc6e164b144bec76f",
+    "url": "/Restaurant-project/static/media/5.a722c9d1.jpg"
+  },
+  {
+    "revision": "aee2a7bdd010d29c0460c8c40339fc1a",
+    "url": "/Restaurant-project/static/media/5.aee2a7bd.jpg"
+  },
+  {
+    "revision": "38de59b1f5693ecdc230c6bc8d3ff142",
+    "url": "/Restaurant-project/static/media/6.38de59b1.jpg"
+  },
+  {
+    "revision": "aedc387435461405c5344a7800e7eb8a",
+    "url": "/Restaurant-project/static/media/6.aedc3874.jpg"
+  },
+  {
+    "revision": "cdff3418cb5e3b9bc272825077078d69",
+    "url": "/Restaurant-project/static/media/6.cdff3418.jpg"
+  },
+  {
+    "revision": "1156c104fff1ff0dbf77f40ceb5e0bf4",
+    "url": "/Restaurant-project/static/media/7.1156c104.jpg"
+  },
+  {
+    "revision": "c08c83f93ee3e399348c5171d3f37840",
+    "url": "/Restaurant-project/static/media/7.c08c83f9.jpg"
+  },
+  {
+    "revision": "ff12dbd7ddcd0d8695da492447626d7a",
+    "url": "/Restaurant-project/static/media/7.ff12dbd7.jpg"
+  },
+  {
+    "revision": "51e20305123308eaaea3ad086a09a2f3",
+    "url": "/Restaurant-project/static/media/8.51e20305.jpg"
+  },
+  {
+    "revision": "b09ba9f481de13477663b1914d639f35",
+    "url": "/Restaurant-project/static/media/8.b09ba9f4.jpg"
+  },
+  {
+    "revision": "53e373792753cca7bcd92768d5f60ed1",
+    "url": "/Restaurant-project/static/media/9.53e37379.jpg"
+  },
+  {
+    "revision": "64d45ff12f68992acd6aac672f1a6733",
+    "url": "/Restaurant-project/static/media/9.64d45ff1.jpg"
+  },
+  {
+    "revision": "88cdcf514f2b9b82263f4f50af4bcf21",
+    "url": "/Restaurant-project/static/media/9.88cdcf51.jpg"
+  },
+  {
+    "revision": "585242d76a09b3f8f6961a366be7ab46",
+    "url": "/Restaurant-project/static/media/concerts.585242d7.jpg"
+  },
+  {
+    "revision": "212e06225ea42ae71bef02987e49037d",
+    "url": "/Restaurant-project/static/media/fifth-slider.212e0622.jpg"
+  },
+  {
+    "revision": "ef0b25ffc89ad3aeaeecb91ce56ec85a",
+    "url": "/Restaurant-project/static/media/first-slider.ef0b25ff.jpg"
+  },
+  {
+    "revision": "dac32cdffb0a8c9f070337dcb829743b",
+    "url": "/Restaurant-project/static/media/fourth-slider.dac32cdf.jpg"
+  },
+  {
+    "revision": "457843a5500d56b03e0b9db32ac463d0",
+    "url": "/Restaurant-project/static/media/second-slider.457843a5.jpg"
+  },
+  {
+    "revision": "e10029f72c2f9cadf59418c9a16e4e97",
+    "url": "/Restaurant-project/static/media/sports.e10029f7.jpg"
+  },
+  {
+    "revision": "4ab2563edccd0a5233637c246081d0fc",
+    "url": "/Restaurant-project/static/media/third-slider.4ab2563e.jpg"
+  }
+]);
